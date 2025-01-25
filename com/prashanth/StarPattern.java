7c3de0702c77b2751a5343c9b7145db39a149b2d
prashanth.reddiyappa <prashanth.83829@gmail.com>
2025-01-25 22:22:28 +0530

    Commiting star and number pattern java files.

---
+++ com/prashanth/StarPattern.java
@@ -0,0 +1,13 @@
+package com.prashanth;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
